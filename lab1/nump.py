a61130fb5deb7c49376f1a072851dfeea6db1252
Mat <106345025+nocush@users.noreply.github.com>
2022-10-18 22:55:54 +0200

    lab 1

--- lab1/nump.py
+++ lab1/nump.py
@@ -29,13 +29,40 @@ zad6 = (vals>vals.mean(axis=0)).sum(axis=0)
 max_value = vals.max()
 col_max = vals.max(axis=0)
 cols2 = np.array(cols)
-cols2[col_max == max_value]
+zad7 = cols2[col_max == max_value]
 
 #zadanie 1.3
 x = np.arange(-5,5,0.01)
 y = np.tanh(x)
-wyk1 = plt.plot(x,y)
+#wyk1 = plt.plot(x,y)
 
 #
-plt.plot(x[x>0],x[x>0])
+wyk1 = plt.plot(x[x>0],x[x>0])
 plt.plot(x[x<=0],np.exp(x[x<=0])-1)
+
+#praca domowa
+#zadanie 1.2.8
+colzero = (vals == 0).sum(axis=0)
+cols3 = np.array(cols)
+zad8 = cols3[np.argmax(colzero)]
+#zadanie 1.2.9
+sum1 = arr1.sum(axis=0)
+sum2 = arr2.sum(axis=0)
+cols4 = np.array(cols)
+zad9 = cols4[sum1>sum2]
+
+#zadanie 1.3 wykres 2
+y2 = (np.exp(x) - np.exp(-x))/(np.exp(x)+np.exp(-x))
+plt.title("Wykres2")
+wyk2 = plt.plot(x,y2)
+
+#zadanie 1.3 wykres 3
+y3 = 1/(1+np.exp(-x))
+plt.title("Wykres3")
+wyk3 = plt.plot(x,y3)
+
+#zadanie 1.3 wykres 4
+x = np.arange(-5,5,0.01)
+plt.title("Wykres4")
+wyk4 = plt.plot(x[x>0],x[x>0])
+wyk5 = plt.plot(x[x<=0],0*x[x<=0])
